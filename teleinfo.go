bb46f077c0daa77b4073fb8e6fbaf4cd3729c07d
Sébastien Bonnefoy <seb@audreyetseb.org>
2015-10-29 19:22:36 +0100

    Set-up teleinfo frames decoding

--- teleinfo.go
+++ teleinfo.go
@@ -2,16 +2,90 @@ package teleinfo
 
 import (
 	"github.com/tarm/serial"
+	"log"
 )
 
 type Info struct {
-	port serial.Port
+	port    *serial.Port
+	cb      func(string, string)
+	state   string
+	current string
+	key     string
+	value   string
+	ck      string
+	cks     int
 }
 
-func (i *Info) init() {
-
+func (i *Info) init() error {
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 4200}
+	port, err := serial.OpenPort(c)
+	i.port = port
+	if err != nil {
+		log.Fatal(err)
+	}
+	i.state = "INIT"
+	i.cks = 0
+	return err
 }
 
 func (i *Info) read() (string, string) {
 	return "", ""
+}
+
+func (i *Info) SetCB(cb func(string, string)) {
+	i.cb = cb
+}
+
+func (i *Info) decodeSeparator(b byte) {
+	switch i.state {
+	case "KEY":
+		i.key = i.current
+		i.current = ""
+		i.state = "VALUE"
+		if b == ' ' {
+			i.cks = i.cks + int(b)
+		}
+	case "VALUE":
+		i.value = i.current
+		i.current = ""
+		i.state = "CK"
+	case "CK":
+		i.ck = i.current
+		i.current = ""
+		i.state = "KEY"
+	}
+}
+
+func (i *Info) decodeField(b byte) {
+	i.current = i.current + string(b)
+	switch i.state {
+	case "KEY":
+		i.cks = i.cks + int(b)
+	case "VALUE":
+		i.cks = i.cks + int(b)
+	}
+}
+
+func (i *Info) Decode(b byte) {
+	switch b {
+	case '\002':
+		i.state = "KEY"
+	case '\003':
+		if i.ck == string((i.cks&0x3F)+0x20) {
+			i.cb(i.key, i.value)
+		}
+		i.cks = 0
+		i.state = "KEY"
+	case '\n':
+		i.decodeSeparator(b)
+		if i.ck == string((i.cks&0x3F)+0x20) {
+			i.cb(i.key, i.value)
+		}
+		i.cks = 0
+		i.state = "KEY"
+	case ' ':
+		i.decodeSeparator(b)
+	default:
+		i.decodeField(b)
+	}
 }
--- teleinfo_test.go
+++ teleinfo_test.go
@@ -1,8 +1,35 @@
 package teleinfo
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestFirst(t *testing.T) {
+func xTestFirst(t *testing.T) {
 	var ti Info
 	ti.init()
+}
+
+func TestDecode(t *testing.T) {
+	expected := map[string]string{
+		"MOTDETAT": "000000",
+		"ADCO":     "020828337598",
+		"OPTARIF":  "BBR(",
+		"ISOUSC":   "45",
+	}
+	got := make(map[string]string)
+	cb := func(code string, value string) {
+		got[code] = value
+	}
+	var ti Info
+	ti.SetCB(cb)
+
+	frame := "\002MOTDETAT 000000 B\nADCO 020828337598 N\nOPTARIF BBR( S\nISOUSC 45 ?\nBBRHCJB 012133887 >\nBBRHPJB 038554302 H\nBBRHCJW 002903317 K\nBBRHPJW 003800290 U\nBBRHCJR 001504374 E\nBBRHPJR 000907447 Y\nPTEC HPJB P\nDEMAIN BLEU V\nIINST 004 [\nIMAX 049 L\nPAPP 01100 #\nHHPHC Y D\n\003"
+	for i := 0; i < len(frame); i++ {
+		ti.Decode(frame[i])
+	}
+	for code, value := range expected {
+		if got[code] != value {
+			t.Error("Value for ", code, " is ", got[code], " instead of ", value)
+		}
+	}
 }
